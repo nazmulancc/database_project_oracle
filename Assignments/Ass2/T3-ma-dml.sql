cf1abde4b8abe7c0e68b9a450b9bc6b9db34dde3
Nazmul Hasan <nhas0020@student.monash.edu>
2025-04-02 23:29:43 +1100

    Ass2: T3 A and B complete.

--- Assignments/Ass2/T3-ma-dml.sql
+++ Assignments/Ass2/T3-ma-dml.sql
@@ -2,26 +2,46 @@
 -- ITO4132
 --T3-ma-dml.sql
 
---Student ID:
---Student Name:
+-- Student ID: 34273654
+-- Student Name: Nazmul Hasan
 
 /* Comments for your marker:
 
-
-
+-- Monash Automotive (MA) Data Insert Script
+student id:34273654
+student name: Nazmul Hasan 
+last modified date: 03.04.2025
 
 */
+
 
 -- Please ensure EACH SQL statement you add is formatted
 -- and includes a semicolon
 
 /* (a) */
 
+DROP SEQUENCE service_seq;
+DROP SEQUENCE part_charge_seq;
 
-
+CREATE SEQUENCE service_seq START WITH 1000 INCREMENT BY 10;
+CREATE SEQUENCE part_charge_seq START WITH 1000 INCREMENT BY 10;
 
 /* (b) */
 
+
+UPDATE vehicle
+SET 
+    veh_rego = 'GDD132'
+WHERE cust_no = (
+    SELECT 
+        cust_no
+    FROM 
+        customer
+    WHERE 
+        upper(cust_phone) = upper('6715573197')
+);
+
+COMMIT;
 
 
 

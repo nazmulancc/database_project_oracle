460ab74da66796252c1c430fdde9409f15697f12
Nazmul Hasan <nhas0020@student.monash.edu>
2025-04-13 14:17:46 +1000

    Ass2: Task 6a complete

--- Assignments/Ass2/T6-ma-json.sql
+++ Assignments/Ass2/T6-ma-json.sql
@@ -5,7 +5,7 @@
 
 -- Student ID: 34273654
 --Student Name: Nazmul Hasan
---last modified date: 07.04.2025
+--last modified date: 13.04.2025
 
 /* Comments for your marker:
 
@@ -17,3 +17,35 @@
 -- PLEASE PLACE REQUIRED SQL SELECT STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
+SELECT 
+    JSON_OBJECT(
+        '_id' VALUE c.cust_no || '_' || v.veh_vin,
+        'customer' VALUE JSON_OBJECT(
+            'custno' VALUE c.cust_no,
+            'name' VALUE c.cust_name,
+            'phone' VALUE c.cust_phone
+        ),
+        'rego_number' VALUE v.veh_rego,
+        'make' VALUE v.veh_make,
+        'model' VALUE v.veh_model,
+        'year' VALUE TO_CHAR(v.veh_year, 'YYYY'),
+        'noservices' VALUE COUNT(s.serv_no),
+        'booked_services' VALUE JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'servno' VALUE s.serv_no,
+                'servdate' VALUE TO_CHAR(s.serv_date, 'DD-Mon-YYYY'),
+                'labourcost' VALUE s.serv_labour_cost,
+                'partcost' VALUE s.serv_parts_cost,
+                'totalcost' VALUE NVL(s.serv_labour_cost, 0) + NVL(s.serv_parts_cost, 0)
+            )
+        )
+    FORMAT JSON) AS json_output
+FROM 
+    customer c
+JOIN 
+    vehicle v ON c.cust_no = v.cust_no
+LEFT JOIN 
+    service s ON v.veh_vin = s.veh_vin
+GROUP BY 
+    c.cust_no, c.cust_name, c.cust_phone, 
+    v.veh_vin, v.veh_rego, v.veh_make, v.veh_model, v.veh_year;
--- Assignments/Ass2/T6-ma-mongo.mongodb.js
+++ Assignments/Ass2/T6-ma-mongo.mongodb.js
@@ -14,8 +14,25 @@
 //Use (connect to) your database - you MUST update xyz001
 //with your authcate username
 
-use("xyz001");
+use("nhas0020");
 
+// db.services.drop();
+// db.services.insertMany([
+
+// {"_id":"1000_WBAWR33598P984354","customer":{"custno":1000,"name":"Andres Syphas","phone":"9571953915"},"rego_number":"I6W872","make":"Toyota","model":"Hilux","year":"2018","noservices":2,"booked_services":[{"servno":106,"servdate":"15-Mar-2023","labourcost":450,"partcost":520.4,"totalcost":970.4},{"servno":101,"servdate":"08-Feb-2023","labourcost":350,"partcost":256.74,"totalcost":606.74}]},
+// {"_id":"1010_WAUVFAFH4AN857561","customer":{"custno":1010,"name":"Charlotta Schimke","phone":"6053395648"},"rego_number":"Z7K189","make":"Mazda","model":"3","year":"2020","noservices":2,"booked_services":[{"servno":102,"servdate":"15-Feb-2023","labourcost":420,"partcost":315.2,"totalcost":735.2},{"servno":109,"servdate":"05-Apr-2023","labourcost":400,"partcost":350.6,"totalcost":750.6}]},
+// {"_id":"1020_WAUGFAFC8FN843179","customer":{"custno":1020,"name":"Cathrine Lynes","phone":"9678403727"},"rego_number":"R9X386","make":"Hyundai","model":"i30","year":"2017","noservices":1,"booked_services":[{"servno":103,"servdate":"22-Feb-2023","labourcost":275,"partcost":189.5,"totalcost":464.5}]},
+// {"_id":"1030_JN1CV6EK6FM569421","customer":{"custno":1030,"name":"Farrel Grazier","phone":"3504231495"},"rego_number":"C3Y667","make":"Mazda","model":"CX-5","year":"2021","noservices":2,"booked_services":[{"servno":104,"servdate":"01-Mar-2023","labourcost":380,"partcost":420.75,"totalcost":800.75},{"servno":1000,"servdate":"21-Mar-2024","labourcost":0,"partcost":0,"totalcost":0}]},
+// {"_id":"1040_3GTU1YEJ1BG975085","customer":{"custno":1040,"name":"Angie Eouzan","phone":"7355393095"},"rego_number":"D1E595","make":"Audi","model":"A4","year":"2016","noservices":0,"booked_services":[{"servno":null,"servdate":null,"labourcost":null,"partcost":null,"totalcost":0}]},
+// {"_id":"1040_5XXGM4A74DG668202","customer":{"custno":1040,"name":"Angie Eouzan","phone":"7355393095"},"rego_number":"Y9B216","make":"Jaguar","model":"XJ6","year":"2015","noservices":1,"booked_services":[{"servno":105,"servdate":"08-Mar-2023","labourcost":320,"partcost":275.3,"totalcost":595.3}]},
+// {"_id":"1040_WA1CGCFE6BD111395","customer":{"custno":1040,"name":"Angie Eouzan","phone":"7355393095"},"rego_number":"Q2V771","make":"BMW","model":"3 Series","year":"2020","noservices":0,"booked_services":[{"servno":null,"servdate":null,"labourcost":null,"partcost":null,"totalcost":0}]},
+// {"_id":"1050_4T1BK3DB4CU320186","customer":{"custno":1050,"name":"Butch Japp","phone":"6715573197"},"rego_number":"GDD132","make":"Mitsubishi","model":"Lancer","year":"2021","noservices":1,"booked_services":[{"servno":107,"servdate":"22-Mar-2023","labourcost":290,"partcost":195.25,"totalcost":485.25}]},
+// {"_id":"1060_19UYA42792A297253","customer":{"custno":1060,"name":"Tasha Obispo","phone":"1803631411"},"rego_number":"K9I306","make":"Volkswagen","model":"Tiguan","year":"2016","noservices":1,"booked_services":[{"servno":108,"servdate":"29-Mar-2023","labourcost":360,"partcost":310.8,"totalcost":670.8}]},
+// {"_id":"1070_19XFB2E59DE245929","customer":{"custno":1070,"name":"Suzi Buxsy","phone":"2338078149"},"rego_number":"J9Z085","make":"Nissan","model":"Maxima","year":"2019","noservices":1,"booked_services":[{"servno":110,"servdate":"12-Apr-2023","labourcost":280,"partcost":230.9,"totalcost":510.9}]},
+// {"_id":"1080_TRUWT28N921709039","customer":{"custno":1080,"name":"Fredra Doulton","phone":"5625108047"},"rego_number":"H6D767","make":"Ford","model":"Focus","year":"2017","noservices":0,"booked_services":[{"servno":null,"servdate":null,"labourcost":null,"partcost":null,"totalcost":0}]},
+// {"_id":"1080_WA1DGAFE9CD490452","customer":{"custno":1080,"name":"Fredra Doulton","phone":"5625108047"},"rego_number":"F4I963","make":"Ford","model":"Falcon","year":"2014","noservices":0,"booked_services":[{"servno":null,"servdate":null,"labourcost":null,"partcost":null,"totalcost":0}]}
+
+// ]);
 // (b)
 // PLEASE PLACE REQUIRED MONGODB COMMAND TO CREATE THE COLLECTION HERE
 // YOU MAY PICK ANY COLLECTION NAME
